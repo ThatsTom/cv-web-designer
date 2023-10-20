2a96ec1fc6a328a8f269a935f9a6c20ef493af72
ThatsTom <tomholanda300@gmail.com>
2023-10-19 23:40:27 -0300

    atualização de descricoes

--- assets/js/conteuModal.js
+++ assets/js/conteuModal.js
@@ -27,8 +27,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const openModalButtons = document.querySelectorAll(".open-modal");
     const textoExperiencia = ["O foco principal é o Marketing Digital da empresa, toda a edição do site e uma grande parte das imagens dos produtos foi feita por mim, implementação de API de serviços de entrega e pagamentos. Criei bots para tarefas repetitivas usando python, para poder manipular e atualizar o sistema ERP(Tiny) de forma que evite erros. Cadastro de produtos voltados para o E-commerce e as plataformas integradas como Mercado Livre, Shopee, Magalu, Tray, criando imagens unicas, descrições remodeladas, SEO tanto para buscas nas plataformas como na proprias tags do site, tirava fotos e fazia video dos produtos e editava com Photoshop, Coreldraw, Canva e Premier. Gerenciamento de todo conteudo plataforma Tray, fazendo toda otimização nas descrições e imagens para subir para o site, gerenciamento de campanhas voltadas para gerar leads para o site, leitura de todo as metricas do trafego pago(google ads, instagram e facebook), cadastrava produtos de diversos formatos para cada plataforma de acordo com a demanda",
   "A agência me manda o projeto em figma com tudo que precisa ser feito, eu transformo todo o projeto em aplicações web, fazendo toda a integração e configurações para o wordpress, seguindo os padrões exigidos pelo projeto se atentando a todos os detalhes, modificando oque se adequá melhor ao projeto e subir todos os arquivos online.",
-  "Texto Bl Esportes",
-  "Texto Rapido Card",
+  "Faço atendimento à clientes presencial e online, para fazer a criação de suas artes, interpretando e auxiliando a suas dúvidas sobre o desenvolvimento do processo criativo, mais especificamente do que é possível fazer de acordo com a demanda exigida, solucionando seus problemas com excelência no atendimento sendo criterioso nos diálogos e sancionando suas dúvidas. Crio moldes físicos e passo eles para vetorização digital, encaixo as artes em moldes digitais usando CorelDraw para reproduzir o layout aprovado em peças de roupas físicas.",
+  "Elaboro o layout em geral de acordo com a demanda como logo, cartão de visita, desenhos técnicos, ilutrações, banners, identidade visual, cracha e artes específicas, utilizando CorelDraw, Photoshop, Illustrator e ferramentas auxiliares. Faço a verificação dos arquivos de pré-impressão para um vistoria antes de produzir se os dados estão todos corretos e conectados de acordo com seu campo com o programa Bartender para poder entrar em produção nas impressoras da equipe inteira de arte. Organizo os arquivos de Excel como toda a documentação e validação de informações variaveis para os projetos. Uso o sistemas TOTVs para organização das operações. Fui responsável de criar metodos para empresa na parte de expedição criando um sistema mais automatico e diminuindo o uso de materias manuais como papel para coisas cotidianas e organização de todo o setor de produção tanto na parte de passar as funções para os funcionários como organização do setor, faço o manuseio de maquinas de produção, responsável pelo rendimento funcionários e contratação e dispensa dos mesmos, auxilio na iniciação do funcionário no setor de arte e produção auxiliando em todo processo da empresa.",
   
   ]
   
@@ -85,16 +85,15 @@ document.addEventListener('DOMContentLoaded', function () {
         const content = button.getAttribute("data-content");
         const title = button.closest(".card").querySelector(".card-title").textContent;
         
-        // Atualize o título e conteúdo do modal com base nos atributos data-content e título
         modalTitle.textContent = title;
         modalContent.innerHTML = getContentByContentName(content);
       });
     });
   
-    // Função para obter o conteúdo com base no nome
     function getContentByContentName(contentName) {
       const contentMap = {
-        "Assistente de E-commerce": `<h4>Programação</h4>
+        "Assistente de E-commerce": `
+        <h4>Programação</h4>
         <ul>
           <li>HTML</li>
           <li>CSS E SASS</li>
@@ -112,12 +111,15 @@ document.addEventListener('DOMContentLoaded', function () {
           <li>CORELDRAW</li>
           <li>CANVA</li>
         </ul>
-        <h4>Gestão</h4>
+        <h4>Programas Adicionais</h4>
         <ul>
           <li>TINY</li>
           <li>TRAY</li>
+          <li>Google Analytic</li>
+          <li>Nubimetrics</li>
         </ul>
-      `,"Web Designer": `<h4>Programação</h4>
+      `,"Web Designer": `
+      <h4>Programação</h4>
       <ul>
       <li>WORDPRESS</li>
       <li>HTML</li>
@@ -127,13 +129,39 @@ document.addEventListener('DOMContentLoaded', function () {
       <ul>
       <li>PHOTOSHOP</li>
       <li>CONVERSOR DE IMAGEM FEITO EM PYTHON</li>`,
-      "Designer de Produção":`2`,
-      "Desenhista Pleno e Gestor de Arte":`3`,
+
+      "Designer de Produção":`
+       <h4>Editor de Imagem</h4>
+      <ul>
+        <li>PHOTOSHOP</li>
+        <li>CORELDRAW</li>
+      </ul>
+      <h4>Impressão dos arquivos</h4>
+      <ul>
+        <li>Plotter</li>
+      </ul>
+      `,
+      "Desenhista Pleno e Gestor de Arte":`
+      <h4>Editor de Imagem</h4>
+      <ul>
+        <li>PHOTOSHOP</li>
+        <li>CORELDRAW</li>
+      </ul>
+      <h4>Programas adicionais</h4>
+      <ul>
+      <li>Excel</li>
+      <li>Bartender Interprise</li>
+      <li>Totvs</li>
+      </ul>
+      <h4>Impressão</h4>
+      <ul>
+      <li>Epson</li>
+      <li>Brother</li>
+      </ul>`,
       "outra-opcao": `
         Outro conteúdo personalizado.
         Este é um exemplo adicional.
       `,
-        // Adicione mais conteúdos conforme necessário
       };
   
       return contentMap[contentName] || "Conteúdo padrão se não for encontrado.";
